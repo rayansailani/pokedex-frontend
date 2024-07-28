0ddc0172013bacf12820af29770c8c03c4d7b3c7
Mohammed Rayan sailani <mohammed.rayan.sailani@sap.com>
2024-07-28 18:24:31 +0530

    feat: frontend application with basic calls working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';import './App.css';
+import Webcam from 'react-webcam';
+
 
 function App() {
+  const webcamRef = useRef(null);
+  const [imageSrc, setImageSrc] = useState(null);
+
+  const capture = () => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImageSrc(imageSrc);
+  };
+
+  const dataURLtoFile = (dataurl, filename) => {
+    let arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8arr], filename, {type:mime});
+  }
+
+  const sendToServer = async () => {
+    if (imageSrc) {
+      const file = dataURLtoFile(imageSrc, 'captured_image.jpg');
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const response = await fetch('https://ytterbic-malory-rayansailani-19e00628.koyeb.app/predict', {
+        method: 'POST',
+        body: formData
+      });
+      const result = await response.json();
+      console.log(result);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Webcam
+        audio={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={320}
+        height={240}
+      />
+      <button onClick={capture}>Capture</button>
+      {imageSrc && (
+        <>
+          <img src={imageSrc} alt="Captured" />
+          <button onClick={sendToServer}>Send to Server</button>
+        </>
+      )}
     </div>
   );
 }
